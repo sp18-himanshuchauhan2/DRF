351826e3219450ce1a6bd94a39dd98d6c8348578
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-06-25 17:04:12 +0530

    created Model ViewSet

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 # from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, APIView
-
+from rest_framework import viewsets, status
 from home.models import Person
 from home.serializers import PeopleSerializer, LoginSerializer
 
@@ -89,16 +89,69 @@ def login(request):
 
 class PersonAPI(APIView):
     def get(self, request):
-        return Response({'message': 'This is a get request'})
+        objs = Person.objects.filter(color__isnull=False)
+        serializer = PeopleSerializer(objs, many=True)
+        return Response(serializer.data)
+    
+        # return Response({'message': 'This is a get request'})
     
     def post(self, request):
-        return Response({'message': 'This is a post request'})
+        data = request.data
+        serializer = PeopleSerializer(data = data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors)
+    
+        # return Response({'message': 'This is a post request'})
     
     def put(self, request):
-        return Response({'message': 'This is a put request'})
+        data = request.data
+        obj = Person.objects.get(id = data['id'])
+        serializer = PeopleSerializer(obj, data = data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors)
+
+        # return Response({'message': 'This is a put request'})
     
     def patch(self, request):
-        return Response({'message': 'This is a patch request'})
+        data = request.data
+        obj = Person.objects.get(id = data['id'])
+        serializer = PeopleSerializer(obj, data = data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors)
+        
+        # return Response({'message': 'This is a patch request'})
     
     def delete(self, request):
-        return Response({'message': 'This is a delete request'})
+        data = request.data
+        obj = Person.objects.get(id=data['id'])
+        obj.delete()
+        return Response({'message': 'person deleted...'})
+
+        # return Response({'message': 'This is a delete request'})
+
+class PersonViewSet(viewsets.ModelViewSet):
+    serializer_class = PeopleSerializer
+    queryset = Person.objects.all()
+
+    #search method - filter...
+    def list(self, request):
+        search = request.GET.get('search')
+        queryset = self.queryset
+
+        if search:
+            queryset = queryset.filter(name__startswith=search) 
+            
+        serializer = PeopleSerializer(queryset, many=True)
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)

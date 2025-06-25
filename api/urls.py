12e38f9189eb5ba90eff3a1f51dfe59890828e2f
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-06-25 17:01:22 +0530

    added routers for Model View Set

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,16 @@
 from home.views import index
-from django.urls import path
-from home.views import person, login, PersonAPI
+from django.urls import path, include
+from home.views import person, login, PersonAPI, PersonViewSet
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'people', PersonViewSet, basename='people')
+urlpatterns = router.urls
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('index/', index),
     path('person/', person),
     path('login/', login),
-    path('person-api/', PersonAPI.as_view())
+    path('person-api/', PersonAPI.as_view()),
 ]

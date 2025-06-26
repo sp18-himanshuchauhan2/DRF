c5d030d7ef9b1e6b55ee53337e75f2fa18839abe
Himanshu Chauhan <himanshu.chauhan2@sparkeighteen.com>
2025-06-26 17:17:40 +0530

    permissions added

--- myDjangoProject/settings.py
+++ myDjangoProject/settings.py
@@ -130,3 +130,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ]
 }
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]   
+}
